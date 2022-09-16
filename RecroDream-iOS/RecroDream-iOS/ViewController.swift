0fad97f63ba6b04f8e8ad9bd01fe3046e1fd2a35
L-j-h-c <ckrgkswnsgh@naver.com>
2022-09-16 18:35:07 +0900

    #Setting - 프로젝트 생성

---
+++ RecroDream-iOS/RecroDream-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecroDream-iOS
+//
+//  Created by Junho Lee on 2022/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
