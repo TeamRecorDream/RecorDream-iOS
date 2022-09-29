2398ac1dfa6d32691ff44c0beb15a6a441a52ff4
L-j-h-c <ckrgkswnsgh@naver.com>
2022-09-29 20:01:24 +0900

    [Feat] #11 - Coordinator Base 구현 및 주석작업 완료

---
+++ RecorDream-iOS/Projects/Modules/RD-Navigator/Sources/CoordinatorBase/Coordinator/CoordinatorFinishOutput.swift
@@ -0,0 +1,14 @@
+//
+//  CoordinatorFinishOut.swift
+//  RouterCoordinator
+//
+//  Created by Junho Lee on 2022/09/29.
+//
+
+import Foundation
+
+
+/// Coordinator가 자신의 플로우를 마쳤을 때 호출됩니다. 대개의 경우 removeDependency()가 호출됩니다.
+protocol CoordinatorFinishOutput {
+    var finishFlow: (() -> Void)? { get set }
+}
