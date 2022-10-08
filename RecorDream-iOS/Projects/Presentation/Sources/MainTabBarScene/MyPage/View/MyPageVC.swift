f8c66bf71764569fbd7d5d4ed60264973388b8e5
L-j-h-c <ckrgkswnsgh@naver.com>
2022-10-09 04:22:27 +0900

    [Design] #16 - 마이페이지 UI 구현 완료

--- RecorDream-iOS/Projects/Presentation/Sources/MainTabBarScene/MyPage/View/MyPageVC.swift
+++ RecorDream-iOS/Projects/Presentation/Sources/MainTabBarScene/MyPage/View/MyPageVC.swift
@@ -63,6 +63,33 @@ public class MyPageVC: UIViewController {
     
     private let timeSettingView = MyPageInteractionView()
         .viewType(.timeSetting)
+    
+    private let guideLabel: UILabel = {
+        let label = UILabel()
+        label.text = "설정한 시간에 푸시알림을 통해 꿈을 바로 기록할 수 있어요!"
+        label.font = RDDSKitFontFamily.Pretendard.regular.font(size: 12.adjustedH)
+        label.textColor = .white.withAlphaComponent(0.4)
+        return label
+    }()
+    
+    private let logoutButton: UIButton = {
+        let bt = UIButton()
+        bt.backgroundColor = RDDSKitAsset.Colors.purple.color
+        bt.setTitle("로그아웃", for: .normal)
+        bt.setTitleColor(.white, for: .normal)
+        bt.titleLabel?.font = RDDSKitFontFamily.Pretendard.semiBold.font(size: 16)
+        bt.layer.cornerRadius = 15
+        return bt
+    }()
+    
+    private let withdrawlButton: UIButton = {
+        let bt = UIButton()
+        bt.setTitle("탈퇴하기", for: .normal)
+        bt.setUnderline()
+        bt.setTitleColor(.white.withAlphaComponent(0.5), for: .normal)
+        bt.titleLabel?.font = RDDSKitFontFamily.Pretendard.regular.font(size: 14)
+        return bt
+    }()
   
     // MARK: - View Life Cycle
     
@@ -85,7 +112,8 @@ extension MyPageVC {
     private func setLayout() {
         self.view.addSubviews(naviBar, profileImageView, nickNameLabel,
                               editButton, emailLabel, pushSettingView,
-                              timeSettingView)
+                              timeSettingView, guideLabel, logoutButton,
+                              withdrawlButton)
         
         naviBar.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
@@ -124,6 +152,22 @@ extension MyPageVC {
             make.top.equalTo(pushSettingView.snp.bottom).offset(18.adjustedH)
             make.height.equalTo(64.adjustedH)
             make.leading.trailing.equalToSuperview().inset(16.adjusted)
+        }
+        
+        guideLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(25.adjusted)
+            make.top.equalTo(timeSettingView.snp.bottom).offset(8.adjusted)
+        }
+        
+        logoutButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16.adjusted)
+            make.height.equalTo(50.adjustedH)
+            make.bottom.equalTo(withdrawlButton.snp.top).offset(-12.adjusted)
+        }
+        
+        withdrawlButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().inset(safeAreaBottomInset() + 18)
         }
     }
 }
