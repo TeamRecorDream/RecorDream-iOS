8d6eb705d4042439ad6e0e1d56d4e045a9c84efd
suyeon <rlatndus9911@naver.com>
2022-10-24 02:02:45 +0900

    [Feat] #21 - 홈 뷰 멘트 라벨 및 로고뷰 UI 구현

--- RecorDream-iOS/Projects/Presentation/Sources/MainTabBarScene/Home/HomeVC.swift
+++ RecorDream-iOS/Projects/Presentation/Sources/MainTabBarScene/Home/HomeVC.swift
@@ -20,12 +20,63 @@ public class HomeVC: UIViewController {
     public var viewModel: HomeViewModel!
   
     // MARK: - UI Components
+
+    private let logoView = DreamLogoView()
+    private let backgroundView = UIImageView(image: RDDSKitAsset.Images.homeBackground.image)
+
+    private var welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "반가워요, 드림님!"
+        label.font = RDDSKitFontFamily.Pretendard.semiBold.font(size: 24.adjusted)
+        label.textColor = RDDSKitColors.Color.white
+        return label
+    }()
+
+    private let desciptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "그동안 어떤 꿈을 꾸셨나요?"
+        label.font = RDDSKitFontFamily.Pretendard.extraLight.font(size: 24.adjusted)
+        label.textColor = RDDSKitColors.Color.white
+        return label
+    }()
   
     // MARK: - View Life Cycle
     
     public override func viewDidLoad() {
         super.viewDidLoad()
         self.bindViewModels()
+        self.setUI()
+        self.setLayout()
+    }
+
+    // MARK: - UI & Layout
+    private func setUI() {
+        self.view.backgroundColor = RDDSKitAsset.Colors.dark.color
+    }
+
+    private func setLayout() {
+        self.view.addSubviews(backgroundView, logoView, welcomeLabel, desciptionLabel)
+
+        backgroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        logoView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(18)
+            $0.height.equalTo(24.adjustedH)
+        }
+
+        welcomeLabel.snp.makeConstraints {
+            $0.top.equalTo(logoView.snp.bottom).offset(44)
+            $0.leading.equalToSuperview().inset(22)
+        }
+
+        desciptionLabel.snp.makeConstraints {
+            $0.top.equalTo(welcomeLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(welcomeLabel.snp.leading)
+        }
+
     }
 }
 
