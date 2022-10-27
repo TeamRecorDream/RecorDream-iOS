9d3ea670243fc106c3315d7b7259689921fcb009
L-j-h-c <ckrgkswnsgh@naver.com>
2022-10-27 15:59:57 +0900

    [Chore] #31 - 임시로 레포지토리에 샘플 데이터 구현

--- RecorDream-iOS/Projects/Data/Sources/Repositories/DefaultDreamWriteRepository.swift
+++ RecorDream-iOS/Projects/Data/Sources/Repositories/DefaultDreamWriteRepository.swift
@@ -25,16 +25,39 @@ public class DefaultDreamWriteRepository {
 }
 
 extension DefaultDreamWriteRepository: DreamWriteRepository {
-    public func writeDreamRecord(request: DreamWriteRequestEntity) -> Observable<DreamWriteEntity?> {
+    public func fetchDreamRecord(recordId: String) -> Observable<DreamWriteEntity> {
         return Observable.create { observer in
-            self.recordService.writeDreamRecord(title: request.title, date: request.date, content: request.content, emotion: request.emotion, genre: request.genre, note: request.note, voice: request.voice)
-                .subscribe(onNext: { entity in
-                    guard let entity = entity else { return observer.onCompleted() }
-                    observer.onNext(entity.toDomain())
-                }, onError: { err in
-                    observer.onError(err)
-                })
-                .disposed(by: self.disposeBag)
+            observer.onNext(.init(main: .init(titleText: "안녕하세요", contentText: "내용입니다", recordTime: 35.0, date: "22.04.03"),
+                                  emotions: [.init(isSelected: true),
+                                             .init(isSelected: false),
+                                             .init(isSelected: false),
+                                             .init(isSelected: true),
+                                             .init(isSelected: false)],
+                                  genres: [.init(isSelected: false),
+                                           .init(isSelected: false),
+                                           .init(isSelected: true),
+                                           .init(isSelected: false),
+                                           .init(isSelected: false),
+                                           .init(isSelected: false),
+                                           .init(isSelected: true),
+                                           .init(isSelected: true),
+                                           .init(isSelected: false),
+                                           .init(isSelected: false)],
+                                  note: .init(noteText: "노트 샘플 내용")))
+            return Disposables.create()
+        }
+    }
+    
+    public func writeDreamRecord(request: DreamWriteRequest) -> Observable<Void> {
+        return Observable.create { observer in
+//            self.recordService.writeDreamRecord(title: request.title, date: request.date, content: request.content, emotion: request.emotion, genre: request.genre, note: request.note, voice: request.voice)
+//                .subscribe(onNext: { entity in
+//                    guard let entity = entity else { return observer.onCompleted() }
+//                    observer.onNext(())
+//                }, onError: { err in
+//                    observer.onError(err)
+//                })
+//                .disposed(by: self.disposeBag)
             return Disposables.create()
         }
     }
