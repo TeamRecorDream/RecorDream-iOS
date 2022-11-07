621ef3f3fce59bef79565f2cdb83a55191a350b9
suyeon <rlatndus9911@naver.com>
2022-11-08 07:03:43 +0900

    [Add] #21 - Array 관련 Extension 추가

---
+++ RecorDream-iOS/Projects/Core/Sources/Foundation+/Array+.swift
@@ -0,0 +1,15 @@
+//
+//  Array+.swift
+//  RD-Core
+//
+//  Created by 김수연 on 2022/10/24.
+//  Copyright © 2022 RecorDream. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+  func safeget(index: Int) -> Element? {
+    return index >= 0 && index < count ? self[index] : nil
+  }
+}
