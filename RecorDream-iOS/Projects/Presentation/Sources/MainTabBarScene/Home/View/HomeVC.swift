d0dbdfffe8c06e1118d929eefd65028627b908cd
suyeon <rlatndus9911@naver.com>
2022-11-08 07:14:46 +0900

    [Feat] #21 - Home 뷰 일부 레이아웃 수정 및 CollectionView 추가

---
+++ RecorDream-iOS/Projects/Presentation/Sources/MainTabBarScene/Home/View/HomeVC.swift
@@ -0,0 +1,138 @@
+//
+//  HomeVC.swift
+//  
+//
+//  Created by 김수연 on 2022/10/23.
+//
+
+import UIKit
+
+import Domain
+import RD_DSKit
+
+import RxSwift
+import SnapKit
+
+/*
+ TODO: 추가 로직 구현
+ 1) 꿈 카드 최대 10개 랜덤
+ 2) 제목 최대 25자, 텍스트 박스 넘어가면 ... + 생략
+ 3) 꿈 내용 텍스트 박스 넘어가면 ... + 생략
+ */
+
+public class HomeVC: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let disposeBag = DisposeBag()
+    public var viewModel: HomeViewModel!
+
+    private var dreamCardCollectionViewAdapter: DreamCardCollectionViewAdapter?
+    
+    private enum Metric {
+        static let logoViewTop = 18.f
+        static let logoViewHeight = 24.f
+
+        static let mainLabelTop = 44.f
+        static let mainLabelLeading = 22.f
+        static let mainLabelSpacing = 8.f
+
+        static let dreamCardTop = 64.f
+        static let dreamCardWidth = 264.f
+        static let dreamCardHeight = 392.f
+
+        static let minimumLineSpacing = 16.f
+    }
+  
+    // MARK: - UI Components
+
+    private let logoView = DreamLogoView()
+    private let backgroundView = UIImageView(image: RDDSKitAsset.Images.homeBackground.image)
+
+    private var welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "반가워요, 드림님!"
+        label.font = RDDSKitFontFamily.Pretendard.semiBold.font(size: 24.adjusted)
+        label.textColor = RDDSKitColors.Color.white
+        return label
+    }()
+
+    private let desciptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "그동안 어떤 꿈을 꾸셨나요?"
+        label.font = RDDSKitFontFamily.Pretendard.extraLight.font(size: 24.adjusted)
+        label.textColor = RDDSKitColors.Color.white
+        return label
+    }()
+
+    private lazy var dreamCardCollectionView: UICollectionView = {
+        let layout = CarouselLayout()
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: layout
+        )
+        layout.itemSize = CGSize(width: Metric.dreamCardWidth, height: Metric.dreamCardHeight)
+        return collectionView
+    }()
+  
+    // MARK: - View Life Cycle
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.bindViewModels()
+        self.setUI()
+        self.setLayout()
+        self.setCollectionViewAdapter()
+    }
+
+    // MARK: - UI & Layout
+    private func setUI() {
+        self.view.backgroundColor = RDDSKitAsset.Colors.dark.color
+    }
+
+    private func setLayout() {
+        self.view.addSubviews(backgroundView, logoView, welcomeLabel, desciptionLabel, dreamCardCollectionView)
+
+        backgroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        logoView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(Metric.logoViewTop)
+            $0.height.equalTo(Metric.logoViewHeight)
+        }
+
+        welcomeLabel.snp.makeConstraints {
+            $0.top.equalTo(logoView.snp.bottom).offset(Metric.mainLabelTop)
+            $0.leading.equalToSuperview().inset(Metric.mainLabelLeading)
+        }
+
+        desciptionLabel.snp.makeConstraints {
+            $0.top.equalTo(welcomeLabel.snp.bottom).offset(Metric.mainLabelSpacing)
+            $0.leading.equalTo(welcomeLabel.snp.leading)
+        }
+
+        dreamCardCollectionView.snp.makeConstraints {
+            $0.top.equalTo(desciptionLabel.snp.bottom).offset(Metric.dreamCardTop)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(Metric.dreamCardHeight)
+        }
+
+    }
+}
+
+// MARK: - Methods
+
+extension HomeVC {
+  
+    private func bindViewModels() {
+        let input = HomeViewModel.Input()
+        // let output = self.viewModel.transform(from: input, disposeBag: self.disposeBag)
+    }
+
+    private func setCollectionViewAdapter() {
+        self.dreamCardCollectionViewAdapter = DreamCardCollectionViewAdapter(
+            collectionView: self.dreamCardCollectionView, adapterDataSource: HomeViewModel())
+    }
+}
