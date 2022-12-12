861b631616d0e4d8dcd2e51915ef899efe3b0abf
L-j-h-c <ckrgkswnsgh@naver.com>
2022-12-13 01:41:12 +0900

    [Refactor] #56 - Project Template 메서드 리팩토링 및 기능 분리

---
+++ RecorDream-iOS/Tuist/ProjectDescriptionHelpers/BaseInfoPlist.swift
@@ -0,0 +1,44 @@
+import ProjectDescription
+
+public extension Project {
+    static let baseinfoPlist: [String: InfoPlist.Value] = [
+        "CFBundleShortVersionString": "1.0.0",
+        "CFBundleVersion": "1",
+        "CFBundleIdentifier": "com.RecorDream.Release",
+        "UILaunchStoryboardName": "LaunchScreen",
+        "CFBundleDisplayName": "레코드림",
+        "LSApplicationQueriesSchemes": ["kakaokompassauth", "kakaolink"],
+        "CFBundleURLTypes": ["CFBundleURLSchemes": ["kakao$(KAKAO_APP_KEY)"]],
+        "UIApplicationSceneManifest": [
+            "UIApplicationSupportsMultipleScenes": false,
+            "UISceneConfigurations": [
+                "UIWindowSceneSessionRoleApplication": [
+                    [
+                        "UISceneConfigurationName": "Default Configuration",
+                        "UISceneDelegateClassName": "$(PRODUCT_MODULE_NAME).SceneDelegate"
+                    ],
+                ]
+            ]
+        ],
+        "UIStatusBarStyle": "UIStatusBarStyleLightContent",
+        "UIViewControllerBasedStatusBarAppearance": false,
+        "UIAppFonts": [
+            "Item 0": "Pretendard-Black.otf",
+            "Item 1": "Pretendard-Bold.otf",
+            "Item 2": "Pretendard-ExtraBold.otf",
+            "Item 3": "Pretendard-ExtraLight.otf",
+            "Item 4": "Pretendard-Light.otf",
+            "Item 5": "Pretendard-Medium.otf",
+            "Item 6": "Pretendard-Regular.otf",
+            "Item 7": "Pretendard-SemiBold.otf",
+            "Item 8": "Pretendard-Thin.otf"
+        ],
+        "App Transport Security Settings": ["Allow Arbitrary Loads": true],
+        "NSAppTransportSecurity": ["NSAllowsArbitraryLoads": true],
+        "NSMicrophoneUsageDescription": "음성을 통해 꿈을 기록하기 위해서는 마이크 이용 권한이 필요합니다.",
+        "Supports opening documents in place": true,
+        "Application supports iTunes file sharing": true,
+        "ITSAppUsesNonExemptEncryption": false,
+        "UIBackgroundModes": ["fetch", "remote-notification"]
+    ]
+}
