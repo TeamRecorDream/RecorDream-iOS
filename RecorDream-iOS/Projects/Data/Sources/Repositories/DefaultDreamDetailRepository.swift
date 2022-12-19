b237ec7260fcea0f68fea8fb708472f33509bb78
suyeon <rlatndus9911@naver.com>
2022-12-20 06:49:00 +0900

    [Feat] 상세보기 뷰 로직 구현을 위한 파일 생성

---
+++ RecorDream-iOS/Projects/Data/Sources/Repositories/DefaultDreamDetailRepository.swift
@@ -0,0 +1,23 @@
+//
+//  DefaultDreamDetailRepository.swift
+//  
+//
+//  Created by 김수연 on 2022/12/04.
+//
+
+import Domain
+
+import RxSwift
+
+public class DefaultDreamDetailRepository {
+  
+    private let disposeBag = DisposeBag()
+
+    public init() {
+    
+    }
+}
+
+extension DefaultDreamDetailRepository: DreamDetailRepository {
+  
+}
