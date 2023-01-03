4172142b1172b37f634604494dee577923e97f00
suyeon <rlatndus9911@naver.com>
2023-01-04 00:19:13 +0900

    [Chore] Home Entity 타입 변경

--- RecorDream-iOS/Projects/Domain/Sources/Entities/Home/HomeEntity.swift
+++ RecorDream-iOS/Projects/Domain/Sources/Entities/Home/HomeEntity.swift
@@ -9,9 +9,9 @@ import Foundation
 
 public struct HomeEntity {
     public let nickname: String
-    public let records: [Record]?
+    public let records: [Record]
 
-    public init(nickname: String, records: [Record]?) {
+    public init(nickname: String, records: [Record]) {
         self.nickname = nickname
         self.records = records
     }
@@ -23,10 +23,10 @@ public extension HomeEntity {
         public let emotion: Int
         public let date: String
         public let title: String
-        public let genres: [Genre]
+        public let genres: [String]
         public let content: String
 
-        public init(recordId: String, emotion: Int, date: String, title: String, genres: [Genre], content: String) {
+        public init(recordId: String, emotion: Int, date: String, title: String, genres: [String], content: String) {
             self.recordId = recordId
             self.emotion = emotion
             self.date = date
@@ -38,19 +38,17 @@ public extension HomeEntity {
 }
 
 public extension HomeEntity {
-    // 서버에서 Int 배열로 들어오면 장르 케이스 별로 구분해야됨.
-    // 로직 고민중
-    enum Genre: String {
-        case 장르없음 = "#아직 설정되지 않았어요"
-        case 코미디 = "#코미디"
-        case 로맨스 = "#로맨스"
-        case 판타지 = "#판타지"
-        case 공포 = "#공포"
-        case 동물 = "#동물"
-        case 친구 = "#친구"
-        case 가족 = "#가족"
-        case 음식 = "#음식"
-        case 일 = "#일"
-        case 기타 = "#기타"
+    enum Genre: Int {
+        case 장르없음
+        case 코미디
+        case 로맨스
+        case 판타지
+        case 공포
+        case 동물
+        case 친구
+        case 가족
+        case 음식
+        case 일
+        case 기타
     }
 }
