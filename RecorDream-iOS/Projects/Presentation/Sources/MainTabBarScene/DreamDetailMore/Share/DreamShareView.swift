fbf20de0b80ce131c18147cc2328af41daea2b92
suyeon <rlatndus9911@naver.com>
2023-02-02 04:03:09 +0900

    [Chore] DreamShareVC를 View로 변경

--- RecorDream-iOS/Projects/Presentation/Sources/MainTabBarScene/DreamDetailMore/Share/DreamShareView.swift
+++ RecorDream-iOS/Projects/Presentation/Sources/MainTabBarScene/DreamDetailMore/Share/DreamShareView.swift
@@ -1,51 +1,55 @@
 //
-//  DreamShareVC.swift
+//  DreamShareView.swift
 //  Presentation
 //
-//  Created by 김수연 on 2023/01/08.
+//  Created by 김수연 on 2023/02/01.
 //  Copyright © 2023 RecorDream-iOS. All rights reserved.
 //
 
 import UIKit
 
-import Domain
 import RD_DSKit
 
-import RxSwift
 import SnapKit
+import RxSwift
+import RxCocoa
 
-public class DreamShareVC: UIViewController {
-    
+public final class DreamShareView: UIView {
+
     // MARK: - Properties
-    
+
     private let disposeBag = DisposeBag()
-    public var viewModel: DreamShareViewModel!
-    public var factory: ViewControllerFactory!
 
     private enum Metric {
-        static let logoViewTop = 159.f
-        static let logoViewLeadingTrailing = 50.f
+        static let logoViewTop = 159.adjustedH
+        static let logoViewLeadingTrailing = 50.adjustedWidth
 
-        static let cardTop = 22.f
-        static let cardLeadingTrailing = 34.f
+        static let cardTop = 22.adjustedH
+        static let cardLeadingTrailing = 34.adjustedWidth
+        static let cardHeight = 470.adjustedH
 
-        static let emotionImageSize = 85.f
+        static let emotionImageWidth = 85.adjustedWidth
+        static let emotionImageHeight = 85.adjustedH
 
-        static let emotionImageTop = 37.f
-        static let dateLabelTop = 18.f
+        static let emotionImageTop = 37.adjustedH
+        static let dateLabelTop = 18.adjustedH
+        static let contentSpacing = 8.adjustedH
+        static let contentLeadingTrailing = 22.adjustedWidth
         static let dateLabelHeight = 17.f
-        static let contentSpacing = 8.f
-        static let contentLeadingTrailing = 29.f
 
         static let genreStackSpacing = 4.f
-        static let genreStackHeight = 21.f
-        static let noteLabelHeight = 200.f
+        static let genreStackHeight = 21.adjustedH
+        static let noteLabelHeight = 200.adjustedH
+
+        static let voiceNoticeViewWidth = 137.adjustedWidth
+        static let voiceTopSpacing = 9.adjustedH
+        static let voiceNoticeViewHeight = 24.adjustedH
     }
-  
+
     // MARK: - UI Components
 
     private let backgroundView = UIImageView(image: RDDSKitAsset.Images.instaBackground.image)
-    private let instaLogoView = UIImageView(image: RDDSKitAsset.Images.instaRogo.image)
+    let instaLogoView = UIImageView(image: RDDSKitAsset.Images.instaRogo.image)
 
     private let cardView = UIImageView(image: RDDSKitAsset.Images.instaCardRed.image)
 
@@ -89,27 +93,28 @@ public class DreamShareVC: UIViewController {
         label.numberOfLines = 0
         return label
     }()
-  
-    // MARK: - View Life Cycle
-    
-    public override func viewDidLoad() {
-        super.viewDidLoad()
 
-        self.setData(model: DreamDetailEntity(recordId: "", date: "2023/01/08", title: "더미 타이틀", content: "더미 내용", emotion: 1, genre: ["더미장르"], note: "노트", voiceUrl: nil))
+    // MARK: - View Life Cycle
+
+    public init() {
+        super.init(frame: .zero)
 
         self.setUI()
         self.setLayout()
-        self.bindViewModels()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
     // MARK: - UI & Layout
 
     private func setUI() {
-        self.view.backgroundColor = RDDSKitAsset.Colors.dark.color
+        self.backgroundColor = RDDSKitAsset.Colors.dark.color
     }
 
     private func setLayout() {
-        self.view.addSubviews(backgroundView, instaLogoView, cardView)
+        self.addSubviews(backgroundView, instaLogoView, cardView)
 
         backgroundView.snp.makeConstraints {
             $0.edges.equalToSuperview()
@@ -122,6 +127,7 @@ public class DreamShareVC: UIViewController {
 
         cardView.snp.makeConstraints {
             $0.top.equalTo(instaLogoView.snp.bottom).offset(Metric.cardTop)
+            $0.height.equalTo(Metric.cardHeight)
             $0.leading.trailing.equalToSuperview().inset(Metric.cardLeadingTrailing)
         }
 
@@ -130,12 +136,14 @@ public class DreamShareVC: UIViewController {
         emotionImageView.snp.makeConstraints {
             $0.top.equalToSuperview().inset(Metric.emotionImageTop)
             $0.centerX.equalToSuperview()
-            $0.size.equalTo(Metric.emotionImageSize)
+            $0.height.equalTo(Metric.emotionImageHeight)
+            $0.width.equalTo(Metric.emotionImageWidth)
         }
 
         dateLabel.snp.makeConstraints {
             $0.top.equalTo(emotionImageView.snp.bottom).offset(Metric.dateLabelTop)
             $0.centerX.equalToSuperview()
+            $0.height.equalTo(Metric.dateLabelHeight)
         }
 
         titleLabel.snp.makeConstraints {
@@ -156,19 +164,21 @@ public class DreamShareVC: UIViewController {
         }
     }
 
-    func setData(model: DreamDetailEntity) {
-        cardView.image = setEmotionImage(emotion: model.emotion)[0]
-        emotionImageView.image = setEmotionImage(emotion: model.emotion)[1]
+    func setData(emotion: Int, date: String, title: String, content: String, genre: [String]) {
+        cardView.image = setEmotionImage(emotion: emotion)[0]
+        emotionImageView.image = setEmotionImage(emotion: emotion)[1]
 
-        dateLabel.text = model.date
-        titleLabel.text = model.title
-        noteLabel.text = model.content
+        dateLabel.text = date
+        titleLabel.text = title
+        noteLabel.text = content
 
         if genreStackView.subviews.isEmpty {
-            model.genre.forEach {
+            genre.forEach {
                 genreStackView.addArrangedSubview(DreamGenreTagView(type: .detail, genre: $0))
             }
         }
+        titleLabel.addLabelSpacing(kernValue: -0.28)
+        noteLabel.addLabelSpacing(kernValue: -0.12)
     }
 
     private func setEmotionImage(emotion: Int) -> [UIImage] {
@@ -187,15 +197,4 @@ public class DreamShareVC: UIViewController {
             return [RDDSKitAsset.Images.instaCardWhite.image, RDDSKitAsset.Images.feelingLBlank.image]
         }
     }
-}
-
-// MARK: - Methods
-
-extension DreamShareVC {
-  
-    private func bindViewModels() {
-        let input = DreamShareViewModel.Input()
-        let output = self.viewModel.transform(from: input, disposeBag: self.disposeBag)
-    }
-
 }
